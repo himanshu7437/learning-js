1912486cde85f63c848ecedea1ff5009b5052ef2
himanshu7437 <himanshujangra368@gmail.com>
2025-02-12 21:29:58 +0530

    modified

--- 04_control_flow/03_truthy.js
+++ 04_control_flow/03_truthy.js
@@ -1,7 +1,7 @@
 const userEmail = []
 
 if (userEmail) {
-    console.log("Got user email");
+    .log("Got user email");
 } else {
     console.log("Don't have user email");
 }
@@ -20,7 +20,7 @@ if (userEmail) {
 const emptyObj = {}
 
 if (Object.keys(emptyObj).length === 0) {
-    console.log("Object is empty");
+    .log("Object is empty");
 }
 
 // Nullish Coalescing Operator (??): null undefined
@@ -33,11 +33,11 @@ val1 = null ?? 10 ?? 20
 
 
 
-console.log(val1);
+.log(val1);
 
 // Terniary Operator
 
 // condition ? true : false
 
 const iceTeaPrice = 100
-iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
+iceTeaPrice <= 80 ? .log("less than 80") : .log("more than 80")
